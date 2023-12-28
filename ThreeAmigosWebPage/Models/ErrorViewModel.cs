0e9a892bdb7fefcb80e0157337c09793c9496472
Will <88620242+willSherry@users.noreply.github.com>
2023-12-28 22:15:59 +0000

    Recreated ThreeAmigosWebPage using Model View Controller template

---
+++ ThreeAmigosWebPage/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ThreeAmigosWebPage.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
