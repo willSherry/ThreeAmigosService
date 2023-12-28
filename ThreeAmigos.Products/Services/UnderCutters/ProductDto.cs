d94c919f775a754c8f7eba69863f8f78791cb6a2
Will <88620242+willSherry@users.noreply.github.com>
2023-12-28 21:49:24 +0000

    Added extra parameters to undercutters request

--- ThreeAmigos.Products/Services/UnderCutters/ProductDto.cs
+++ ThreeAmigos.Products/Services/UnderCutters/ProductDto.cs
@@ -7,4 +7,20 @@ public class ProductDto
     public int Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
+
+    public float Price { get; set; }
+
+    public string BrandName { get; set; } = string.Empty;
+
+    public int BrandId { get; set; }
+
+    public string CategoryName { get; set; } = string.Empty;
+
+    public int CategoryId { get; set; }
+    
+    
+    public bool InStock { get; set; }
+
+
+    //public DateOnly ExpectedRestock { get; set; }
 }
